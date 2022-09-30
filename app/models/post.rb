bf81eaa2b42265a531d7077915bb677647b21e39
Newton Ombese <newtonombese1@gmail.com>
2022-09-30 11:47:17 +0300

    I'll take more time learning more about validation it is sweet

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    CATEGORY = ['Fiction', 'Non-Fiction']
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {
+        in:(CATEGORY),
+    }
+    validate :title_should_not_contain
+
+    def title_should_not_contain
+        if title == "True Facts"
+            errors.add(:title, "title is not permitted")
+        end
+    end
+
 end
